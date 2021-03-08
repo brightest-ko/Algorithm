f099e311f1f7fbdb4baa257130d42a4ecd6894d8
HEEJO\user <brightest95hj@gmail.com>
2021-03-08 23:43:23 +0900

    구슬탈출2 시작

---
+++ Algorithm-boj/2021 상반기 코딩테스트 준비/2021 상반기 코딩테스트 준비/13460_구슬탈출2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+#define MAX_ 20
+
+int arr[MAX_][MAX_] = {};
+int n, m;
+
+
+int main() {
+	
+	cin >> n >> m;
+	cout << n << " Hello World! " << m <<endl;
+	return 0;
+}
